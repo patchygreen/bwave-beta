72002a2d1a19e8be337e4499e536954e6ad2421b
Patrick Crean <patrick.crean+zcn@zalando.ie>
2026-04-13 15:14:06 +0100

    Remove deprecated experimental.instrumentationHook from Next.js 16
    In Next.js 16, instrumentationHook is enabled by default and
    no longer needs to be in experimental config. Removing it
    eliminates build warnings about unrecognized config keys.

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const { withSentryConfig } = require('@sentry/nextjs')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    instrumentationHook: true,
-  },
 }
 
 module.exports = withSentryConfig(nextConfig, {
